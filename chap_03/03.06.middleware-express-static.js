fe69d361afc8b6f20c68aec3dddc26739e8cdbf3
Elom Atsou Agboka <opentech.consult@gmail.com>
2024-01-15 16:54:06 +0000

    Express middleware
    Use express static middleware to serve static files out of a directory called public

---
+++ chap_03/03.06.middleware-express-static.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('node:path');
+const http = require('node:http');
+
+const app = express();
+
+// sets up the public path, using Node's path module
+const publicPath = path.resolve(__dirname, "public");
+
+// send static files from the publicPath directory
+app.use(express.static(publicPath));
+
+app.use(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Look like you didn't find a static file");
+})
+
+http.createServer(app).listen(3000);
+
