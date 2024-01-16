650061e0578f54bd9acbc9308b2716ba3f53f060
Elom Atsou Agboka <opentech.consult@gmail.com>
2024-01-16 20:59:45 +0000

    Express routing
    Adding conviences to response and request objects. Enrich them with new convinience properties.

---
+++ chap_03/03.13.routing-sendfile.js
@@ -0,0 +1,55 @@
+const http = require('node:http');
+const path = require('node:path');
+
+const express = require('express');
+var logger = require('morgan');
+
+// instantiate an express app
+const app = express();
+
+// log all incoming request
+app.use(logger('short'));
+
+// sets up static file middleware
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+// called when a request to the root is made
+app.get('/', function (request, response) {
+    response.end('Welcome to my home page');
+});
+
+// called when a request to the /aboud is made
+app.get('/about', function(request, response) {
+    response.end('Welcome to the about page');
+});
+
+// called when a request to /weather is made
+app.get('/weather', function(request, response) {
+    response.end('The current weather is NICE!');
+});
+
+//  specifies that the hello part of the route is fixed
+app.get('/hello/:who', function (request, response) {
+    response.end('Hello, ' + request.params.who + '.');
+});
+
+app.get('/world', function (request, response) {
+    response.redirect('/hello/world');
+});
+
+app.get('/express', function (request, response) {
+    response.redirect('http://expressjs.com');
+});
+
+app.get('/video', function (request, response) {
+    const videoPath = path.resolve(__dirname, 'source-code.mkv');
+    response.sendFile(videoPath);
+});
+
+app.use(function(request, response) {
+    response.statusCode = 404;
+    response.end('404!');
+});
+
+http.createServer(app).listen(3000);
